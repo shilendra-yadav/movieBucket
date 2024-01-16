603ac48ac549f88b94962d1ee65c71b71e9e783d
Shilendra Yadav <shilendra.yadav@192.168.1.3>
2024-01-16 09:31:54 +0530

    update key variable name

--- src/Utils/constants.js
+++ src/Utils/constants.js
@@ -1,4 +1,4 @@
-const API_KEY= process.env.API_KEY;
+const API_KEY= process.env.REACT_APP_API_KEY;
 
 export const TRENDING_API = (page) => `https://api.themoviedb.org/3/trending/all/day?api_key=${API_KEY}&page=${page}`;
 export const CREDITS_API = (media_type, id) =>`https://api.themoviedb.org/3/${media_type}/${id}/credits?api_key=${API_KEY}&language=en-US`;
